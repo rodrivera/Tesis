14925a41d9c3d6c797d7664f4339b725d97c2eae
Rodrigo Rivera <rodrivera@udec.cl>
2017-12-06 18:10:44 -0300

    new test script added

--- Interval_vs/Scripts/CCompile.sh
+++
@@ -1,1 +0,0 @@
-g++ -g -std=c++11 -O3 -DNDEBUG -I ~/include -L ~/lib src/C1Test.cpp -o c1test.out -lsdsl -ldivsufsort -ldivsufsort64
--- Interval_vs/Scripts/SCompile.sh
+++
@@ -1,1 +0,0 @@
-g++ -std=c++11 -I src/includes/schmidt/OGDF/ -L src/includes/schmidt/OGDF/_release/ src/includes/schmidt/FastStabbing.cpp src/TEST.cpp -lOGDF -o TEST.out
